724f05cdc5b215033bd1ce327db1cef13861fc4c
ToasterUwU <ToasterGER@gmail.com>
2022-01-18 22:53:49 +0100

    another fix

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,7 +1,7 @@
 import nextcord
 from nextcord.ext import commands
 
-from ..internal_tools.discord_functions import *
+from internal_tools.discord_functions import *
 
 
 class Owner(commands.Cog):
