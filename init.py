e050a054a1244d42eb6701ad02894fd83dfd2ae7
ToasterUwU <toasterger@gmail.com>
2023-02-07 02:20:04 +0100

    update for use on linux

--- init.py
+++ init.py
@@ -6,7 +6,7 @@ from subprocess import Popen
 
 DETACHED_PROCESS = 0x00000008
 
-parent_dir = __file__.rsplit(".base\\", 1)[0]
+parent_dir = __file__.rsplit(".base/", 1)[0]
 
 dir_name = input("Name of the Directory: ")
 if not os.path.exists(parent_dir + dir_name):
@@ -22,20 +22,21 @@ if not os.path.exists(parent_dir + dir_name):
 
 time.sleep(1)
 
-with open(parent_dir + dir_name + "\\bot_setup.iss", "r") as f:
+with open(parent_dir + dir_name + "/bot_setup.iss", "r") as f:
     text = f.read()
 
-with open(parent_dir + dir_name + "\\bot_setup.iss", "w") as f:
+with open(parent_dir + dir_name + "/bot_setup.iss", "w") as f:
     text = text.replace("Base Bot", f"{dir_name} Bot")
     text = text.replace("%APP_ID%", str(uuid.uuid4()))
     f.write(text)
 
 p = Popen(
-    f'code.cmd "{parent_dir + dir_name}"',
+    f'nohup code "{parent_dir + dir_name}"',
     shell=True,
     stdin=None,
     stdout=None,
     stderr=None,
     close_fds=True,
     creationflags=DETACHED_PROCESS,
+    preexec_fn=os.setpgrp,
 )
