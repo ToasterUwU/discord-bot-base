a08a0cdf029e3471da4e7b9a93329e4b35872f8a
ToasterUwU <toasterger@gmail.com>
2023-02-07 02:28:32 +0100

    hello linux

--- clean.bat
+++
@@ -1,2 +0,0 @@
-del /s /q bot.spec bot.log .timetracker source_code.zip windows_installer.exe .\data\*.json
-rmdir /s /q dist\ build\ __pycache__\ cogs\__pycache__ internal_tools\__pycache__
---
+++ clean.sh
@@ -0,0 +1,2 @@
+rm -f bot.spec bot.log .timetracker source_code.zip windows_installer.exe ./data/*.json
+rm -rf dist/ build/ __pycache__/ cogs/__pycache__ internal_tools/__pycache__
